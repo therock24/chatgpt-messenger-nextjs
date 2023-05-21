f21df33884a07f7d7cc8b2bd8d45d991c665db72
therock24 <vitorlamela@gmail.com>
2023-05-21 12:22:34 +0100

    initial commit with just title and some content

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="flex">
+          { /* Sidebar */}
+
+          {/* ClientProvider - Notifications */}
+        
+          <div className='bg-[#343541] flex-1'>
+          {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
