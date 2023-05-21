17b1d067a864ed87ea1b0224269165ef9f39e06a
therock24 <vitorlamela@gmail.com>
2023-05-21 12:51:03 +0100

    feat: initial chatgpt page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { SunIcon } from '@heroicons/react/24/outline'
+import { SunIcon, ExclamationTriangleIcon, BoltIcon  } from '@heroicons/react/24/outline'
 
 function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
         <h1 className="text-5xl font-bold mb-20">ChatGPT Messenger Next.js</h1> 
-        <div>
+        <div className='flex space-x-2 text-center'>
             <div>
                 <div className="flex flex-col items-center justify-center mb-5">
                     <SunIcon className="h-8 w-8" />
@@ -14,6 +14,28 @@ function HomePage() {
                     <p className="infoText">"Explain something to me"</p>
                     <p className="infoText">"What is the difference between a mouse and a cat"</p>
                     <p className="infoText">"What is the color of the sun?"</p>
+                </div>
+            </div>
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <BoltIcon className="h-8 w-8" />
+                    <h2>Capabilities</h2>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">Change the ChatGPT Model to use</p>
+                    <p className="infoText">Messages are store in Firebase Firestore</p>
+                    <p className="infoText">Toast notifications when ChatGPT is working</p>
+                </div>
+            </div>
+            <div>
+                <div className="flex flex-col items-center justify-center mb-5">
+                    <ExclamationTriangleIcon className="h-8 w-8" />
+                    <h2>Limitations</h2>
+                </div>
+                <div className="space-y-2">
+                    <p className="infoText">May sometimes generate incorrect information</p>
+                    <p className="infoText">May sometimes produce harmful instructions </p>
+                    <p className="infoText">Limited knowledge of world and events after 2021</p>
                 </div>
             </div>
         </div> 
