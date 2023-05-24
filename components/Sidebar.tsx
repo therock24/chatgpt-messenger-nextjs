9b9d5924883b32c1fd8b1ad87616efba0142813f
therock24 <vitorlamela@gmail.com>
2023-05-24 01:43:00 +0100

    feat: added authentication with google

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import React from 'react'
 import NewChat from './NewChat'
+import { useSession, signOut } from 'next-auth/react'
 
 function Sidebar() {
+    const { data: session} = useSession();
   return (
     <div className='p-2 flex flex-col h-screen'>
         <div className='flex-1'>
@@ -14,6 +18,15 @@ function Sidebar() {
                 {/* Map through the ChatRows */}
             </div>
         </div>
+        {session && (
+            <img 
+            onClick={() => signOut()}
+            src={session.user?.image!} 
+            alt="Profile pic"
+            className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2
+            hover:opacity-50' />
+        )}
+        <p className='rounded mx-auto mb-2 text-center justify-center'>Sign Out</p>
     </div>
   )
 }
---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+};
