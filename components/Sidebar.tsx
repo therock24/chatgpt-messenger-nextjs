cd44bff7769c58f1a46cfc9e24e4aa36be865387
therock24 <vitorlamela@gmail.com>
2023-05-25 11:16:28 +0100

    feat: added create and delete chat functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,9 +3,21 @@
 import React from 'react'
 import NewChat from './NewChat'
 import { useSession, signOut } from 'next-auth/react'
+import { collection, orderBy, query } from 'firebase/firestore';
+import { useCollection } from "react-firebase-hooks/firestore" 
+import { db } from '@/firebase';
+import ChatRow from './ChatRow';
 
 function Sidebar() {
     const { data: session} = useSession();
+
+    const [chats, loading, error] = useCollection(
+        session && query(collection(db, 'users', session.user?.email!, 'chats'),
+        orderBy("createdAt", "asc")
+    ));
+
+    console.log(chats)
+
   return (
     <div className='p-2 flex flex-col h-screen'>
         <div className='flex-1'>
@@ -16,6 +28,9 @@ function Sidebar() {
                     {/* Model Selections */}
                 </div>
                 {/* Map through the ChatRows */}
+                {chats?.docs.map(chat => (
+                    <ChatRow key={chat.id} id={chat.id} />
+                ))}
             </div>
         </div>
         {session && (
