6ecccff43feea13d1664adf78634087b652b35da
therock24 <vitorlamela@gmail.com>
2023-05-30 17:06:48 +0100

    feat: added chatinput functionality, request to openai API and store response on firestore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,42 @@
-import Sidebar from '@/components/Sidebar'
-import '../styles/globals.css'
-import { SessionProvider } from '@/components/SessionProvider'
-import { getServerSession } from 'next-auth'
-import Login from '@/components/Login'
-import { authOptions } from '@/lib/auth'
+import Sidebar from "@/components/Sidebar";
+import "../styles/globals.css";
+import { SessionProvider } from "@/components/SessionProvider";
+import { getServerSession } from "next-auth";
+import Login from "@/components/Login";
+import { authOptions } from "@/lib/auth";
+import ClientProvider from "@/components/ClientProvider";
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <html lang="en">
       <body>
         <SessionProvider session={session}>
           {!session ? (
-              <Login/>
+            <Login />
           ) : (
             <div className="flex">
-              { /* Sidebar */}
-              <div className='bg-[#202123] max-w-xs h-screen
-              overflow-y-auto md:min-w-[20rem]'>
+              {/* Sidebar */}
+              <div
+                className="bg-[#202123] max-w-xs h-screen
+              overflow-y-auto md:min-w-[20rem]"
+              >
                 <Sidebar />
               </div>
-              
 
               {/* ClientProvider - Notifications */}
-            
-              <div className='bg-[#343541] flex-1'>
-              {children}
-              </div>
+              <ClientProvider />
+
+              <div className="bg-[#343541] flex-1">{children}</div>
             </div>
-          )
-          }
+          )}
         </SessionProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ lib/queryApi.ts
@@ -0,0 +1,25 @@
+import openai from "./chatgpt";
+
+const query = async (prompt: string, chatId: string, model: string) => {
+  // we could have contextual chat by passing the chat ID
+  // in a future implementation
+
+  const res = await openai
+    .createCompletion({
+      model,
+      prompt,
+      temperature: 0.9,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    })
+    .then((res) => res.data.choices[0].text)
+    .catch(
+      (err) =>
+        `ChatGPT was unable to find an answer for that! (Error: ${err.message})`
+    );
+  return res;
+};
+
+export default query;
